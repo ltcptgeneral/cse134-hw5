93818a95de2b77c5a6586e5fe67a3cad749f135b
Arthur Lu <learthurgo@gmail.com>
2023-02-24 17:27:48 -0800

    improve custom dialogs by allowing custom templates to be used
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- scripts/customdialogs.js
+++ scripts/customdialogs.js
@@ -1,31 +1,30 @@
-/*
+/* createDialog creates a new dialog on the page given a template with content, and an optional callback function
+ *			NOTE: the template is assumed to have at least a content tag with id message
+ *
+ * template: query string corresponding to the template element used as the basis for the dialog
  * message: message to display with the dialog
- * cancel: default false - enable the cancel button
- * prompt: default false - enable the input field in the dialog
  * callback: callback function (result, form) called when dialog is closed and 2 parameters:
  * 			result: whether the form was submitted or canceled as true/false
  * 			form: string containing the user input
  * 			NOTE: the callback should not change the dialog or form DOM nodes in any way!!!
  */
-export function createDialog (message, cancel=false, prompt=false, callback=null) {
-	let template = document.querySelector("#dialog-template");
-	let clone = template.content.cloneNode(true);
-	let dialog = clone.querySelector("dialog");
-	let form = clone.querySelector("form");
-	clone.querySelector("#message").innerText = message;
-	if (prompt) {
-		clone.querySelector("#prompt-input").classList.remove("hidden");
-		clone.querySelector("#prompt-input").disabled = false;
-	}
-	if (cancel) {
-		clone.querySelector("#cancel").classList.remove("hidden");
-		clone.querySelector("#cancel").disabled = false;
-	}
+export function createDialog (template, message, callback=()=>{}) {
+	let content = document.querySelector(template).content.cloneNode(true);
+
+	let form = document.createElement("form");
+	form.method = "dialog";
+	form.append(content);
+
+	let dialog = document.createElement("dialog");
+	dialog.append(form);
+
+	dialog.querySelector("#message").innerText = message;
 	if (callback) {
 		dialog.addEventListener("close", () => {
 			callback(dialog.returnValue === "OK", (new FormData(form)).get("prompt-input"));
+			dialog.remove();
 		});
 	}
-	document.body.append(clone);
+	document.body.append(dialog);
 	dialog.showModal();
 }
