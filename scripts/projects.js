5caa708091c25b2d3b3c76de9f7a6474aa881d32
Arthur Lu <learthurgo@gmail.com>
2023-02-27 22:41:14 -0800

    try removing headers

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,8 +3,5 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 const octokit = new Octokit()
   
 await octokit.request('GET /users/ltcptgeneral/repos', {
-	username: 'ltcptgeneral',
-	headers: {
-	  	'X-GitHub-Api-Version': '2022-11-28'
-	}
+	username: 'ltcptgeneral'
 })
